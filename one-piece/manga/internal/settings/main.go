937ec5d5209dd3949930d9935ca0bbe265bf356b
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-03 20:03:34 +0200

    load settings (json) on init

--- one-piece/manga/internal/settings/main.go
+++ one-piece/manga/internal/settings/main.go
@@ -1,10 +1,27 @@
 package settings
 
+import (
+	"encoding/json"
+	"log"
+	"op-manga-dl/internal/data"
+	"os"
+)
+
 var (
 	MaxChapterDownloadsPerDay int
 	DownloadDelay             int
+
+	Settings *data.Settings
 )
 
 func init() {
-	// TODO: load settings
+	file, err := os.Open(data.SettingsLocation)
+	if err != nil {
+		log.Printf("[WARN] Failed to load configuration from \"%s\"!", data.SettingsLocation)
+		return
+	}
+
+	if err := json.NewDecoder(file).Decode(Settings); err != nil {
+		log.Fatalf("[FATAL] %s", err)
+	}
 }
