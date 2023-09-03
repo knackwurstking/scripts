31820f5ba8689c9d5f40cc963437145d26329e84
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-03 20:32:35 +0200

    using colly for web scraping

--- one-piece/manga/internal/data/MangaList.go
+++ one-piece/manga/internal/data/MangaList.go
@@ -1,6 +1,17 @@
 package data
 
+import "time"
+
 // MangaList contains all data for chapters (and arcs).
 type MangaList struct {
+	Time     time.Time  `json:"time"`
 	Chapters []*Chapter `json:"chapters"`
+}
+
+// NewMangaList contains all data for chapters (and arcs).
+func NewMangaList() *MangaList {
+	return &MangaList{
+		Time:     time.Now(),
+		Chapters: make([]*Chapter, 0),
+	}
 }
--- one-piece/manga/internal/scraper/MangaList.go
+++ one-piece/manga/internal/scraper/MangaList.go
@@ -2,11 +2,38 @@ package scraper
 
 import (
 	"fmt"
+	"log"
 	"op-manga-dl/internal/data"
+	"time"
+
+	"github.com/gocolly/colly/v2"
 )
 
-func ParseMangaList() (*data.MangaList, error) {
-	// TODO: parse manga list and return data
+// ParseMangaList
+// :d: (optional) use existing data for parsing the manga list
+func ParseMangaList(d *data.MangaList) (*data.MangaList, error) {
+	if d == nil {
+		d = data.NewMangaList()
+	}
 
-	return nil, fmt.Errorf("Under construction!")
+	d.Time = time.Now()
+
+	c := colly.NewCollector()
+
+	// TODO: css selector to search for
+	c.OnHTML("<css selector>", func(e *colly.HTMLElement) {
+		// ...
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		log.Printf("[DEBUG] request to \"%s\"\n", r.URL)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		log.Printf("[ERROR] %s: %s\n", r.Request.URL, err)
+	})
+
+	c.Visit("http://onepiece-tube.com/manga/kapitel-mangaliste")
+
+	return d, fmt.Errorf("Under construction!")
 }
