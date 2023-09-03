dbed927ad9c4bbbbf7db3a85367fb0bf36547783
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-03 19:17:22 +0200

    add todos

--- one-piece/manga/cmd/op-manga-dl/main.go
+++ one-piece/manga/cmd/op-manga-dl/main.go
@@ -1,5 +1,14 @@
 package main
 
+// TODO: ...
+// ... parse manga list for chapters and arcs (cache in data/chapters.json)
+// ... check for missing chapters in data/downloads (or data/chapters.json)
+// ... get next chapter to download
+// ... parse url to chapter and get all available pages
+// ... download each page (jpg 01-??)
+// ... merge all jpg's to a pdf with ImageMagic (`convert "*.{jpg}" -quality 100 -density 150 "<nr.> <chapter name>.pdf"`)
+// ... mark chapter as complete in "data/chapters.json"
+
 func main() {
 	// ...
 }
