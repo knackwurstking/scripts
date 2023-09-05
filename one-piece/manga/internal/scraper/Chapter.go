60bda8d77f1d8ba808735b8cba5a3386850a0793
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-05 19:03:28 +0200

    add ParseChapter function

--- one-piece/manga/internal/scraper/Chapter.go
+++ one-piece/manga/internal/scraper/Chapter.go
@@ -1,12 +1,48 @@
 package scraper
 
 import (
-	"fmt"
-	"op-manga-dl/internal/data"
+	"encoding/json"
+	"log"
+	Data "op-manga-dl/internal/data"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
 )
 
-func ParseChapter() (data *data.ChapterData, err error) {
-	// TODO: do it just like in ParseMangaList just from some different url with different data
+func ParseChapter(chapterURL string) (data *Data.ChapterData, err error) {
+	data = &Data.ChapterData{}
+	c := colly.NewCollector()
 
-	return nil, fmt.Errorf("under construction")
+	c.OnHTML("script", func(e *colly.HTMLElement) {
+		e.Text = strings.Trim(e.Text, " ")
+		if len(e.Text) < 13 {
+			return
+		}
+
+		if e.Text[0:13] == "window.__data" {
+			e.Text, _ = strings.CutPrefix(e.Text, "window.__data")
+			e.Text = strings.TrimLeft(e.Text, " =")
+			e.Text = strings.TrimRight(e.Text, "; ")
+
+			if err := json.Unmarshal([]byte(e.Text), data); err != nil {
+				log.Printf("[ERROR] %s\n", err)
+			}
+		}
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		log.Printf("[DEBUG] request to \"%s\"\n", r.URL)
+	})
+
+	c.OnError(func(r *colly.Response, e error) {
+		err = e
+	})
+
+	if err := c.Visit(chapterURL); err != nil {
+		return data, err
+	}
+
+	c.Wait()
+
+	return data, err
 }
