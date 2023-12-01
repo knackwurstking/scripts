a968926c1fbf46ce337aee288bb3f3f62bed0346
Udo Bauer <knackwurstking.tux@gmail.com>
2023-12-01 07:23:56 +0100

    update git repo backup script

--- cron-scripts/backup-git-repo.sh
+++ cron-scripts/backup-git-repo.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+COLOR_RESET="\033[0m"
+COLOR_BOLD_YELLOW="\033[1;33m"
+COLOR_BOLD_GREEN="\033[1;32m"
+
 USER=git
-BACKUP_LOCATION=/mnt/media/backups
-BACKUP_LOG=$HOME/.backup.log
 
-echo -e "\033[1;33mRunning backup for user \033[1;32m$USER\033[1;33m on \033[1;32m$(date)\033[0m" >> $BACKUP_LOG
+DST=/mnt/media/backups
 
-tar --create --gzip --preserve-permissions \
-  --file ${BACKUP_LOCATION}/${USER}/backup-$(date +%Y-%m-%d).tar.gz \
-  ${HOME} 2>> $BACKUP_LOG
+LOG_DIR=${HOME}/.log
+LOG=${LOG_DIR}/backup.log
+
+[ ! -e ${LOG_DIR} ] && mkdir ${LOG_DIR}
+
+echo -e \
+    "${COLOR_BOLD_YELLOW}Running backup for user " \
+    "${COLOR_BOLD_GREEN}${USER}" \
+    "${COLOR_BOLD_YELLOW} on " \
+    "${COLOR_BOLD_GREEN}$(date)${COLOR_RESET}" >> ${LOG}
+
+tar --create \
+    --gzip \
+    --preserve-permissions \
+    --file ${DST}/${USER}/backup-$(date +%Y-%m-%d).tar.gz \
+    ${HOME} 2>> ${LOG}
 
 exit 0
