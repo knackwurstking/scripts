f329dd3db54967136707276e0b1a51d158bd544a
Udo Bauer <knackwurstking.tux@gmail.com>
2023-12-04 12:14:09 +0100

    update logs

--- one-piece/manga/cmd/op-manga-dl/main.go
+++ one-piece/manga/cmd/op-manga-dl/main.go
@@ -21,7 +21,7 @@ func main() {
         next := time.Date(now.Year(), now.Month(), now.Day()+1, settings.FetchHour, 0, 0, 0, time.Local)
 
         duration := next.Sub(now)
-        log.Printf("[DEBUG] Sleep a while: %d", duration)
+        log.Printf("sleep a while: %d", duration)
         time.Sleep(duration)
 
         if next.Weekday() == settings.FetchWeekDay {
@@ -31,18 +31,19 @@ func main() {
 }
 
 func downloadAllChapters() {
-    log.Printf("[DEBUG] download all chapters available")
+    log.Printf("download all chapters available")
 
 	ml, err := scraper.ParseMangaList()
 	if err != nil {
-		log.Fatalf("[FATAL] %s", err)
+        log.Printf("error: fetch & parse manga list: %s", err)
+        return
 	}
 
 	for _, chapter := range ml.Chapters {
 		arc, i := ml.GetArc(chapter.ArcId)
 		if arc == nil {
-			log.Fatalf(
-				"[FATAL] Arc for %s with the id %d not found! (This should never happen)",
+            log.Fatalf(
+                "fatal: Arc for %s with the id %d not found! (This should never happen)",
 				chapter.Name, chapter.ArcId,
 			)
 		}
@@ -63,7 +64,7 @@ func downloadAllChapters() {
 
                 newMl, err := scraper.ParseMangaList()
 				if err != nil {
-					log.Printf("[ERROR] %s", err)
+                    log.Printf("error: %s", err)
 				} else {
                     ml = newMl
                 }
@@ -75,48 +76,48 @@ func downloadAllChapters() {
 }
 
 func downloadChapter(chapter scraper.MangaList_Chapter, path string) {
-	log.Printf("[INFO] Download the %d pages to \"%s\"", chapter.Pages, path)
+    log.Printf("download the %d pages: \"%s\"", chapter.Pages, path)
 
 	// download jpg/png from dURL - scrape the same script section like before
 	chapterData, err := scraper.ParseChapter(chapter.Href)
 	if err != nil {
-		log.Printf("[ERROR] %s\n", err)
+        log.Printf("error: parse chapter: %s\n", err)
 		return
 	}
 
 	pages := make([]string, len(chapterData.Chapter.Pages))
 	for i, page := range chapterData.Chapter.Pages {
-		log.Printf("[INFO] Downloading \"%s\"", page.Url)
+        log.Printf("downloading \"%s\"", page.Url)
 		r, err := http.Get(page.Url)
 		if err != nil {
-			log.Printf("[ERROR] Error while downloading page %d: %s", i+1, err)
+            log.Printf("error: error while downloading page %d: %s", i+1, err)
 			return
 		}
 		data, err := io.ReadAll(r.Body)
 		if err != nil {
-			log.Printf("[ERROR] Reading body data for page %d: %s", i+1, err)
+            log.Printf("error: reading body data for page %d: %s", i+1, err)
 			return
 		}
 		if len(data) == 0 {
-			log.Printf("[ERROR] No data to read for page %d", i+1)
+            log.Printf("error: no data to read for page %d", i+1)
 			return
 		}
 		e, _ := utils.GetExtension(page.Type)
 		p := filepath.Join(path, fmt.Sprintf("%02d.%s", i+1, e))
 		err = os.WriteFile(p, data, 0644)
 		if err != nil {
-			log.Printf("[ERROR] Write file \"%s\" failed: %s", p, err)
+            log.Printf("error: write file \"%s\" failed: %s", p, err)
 			return
 		}
 		pages[i] = p
 	}
 
 	if err := utils.ConvertImagesToPDF(path, pages...); err != nil {
-		log.Printf("[ERROR] Convert pages to pdf failed: %s", err)
+        log.Printf("error: Convert pages to pdf failed: %s", err)
 	}
 }
 
 func sleep() {
-	log.Printf("[INFO] Wait %d ms...", settings.DownloadDelay)
+	log.Printf("wait %d ms...", settings.DownloadDelay)
 	time.Sleep(time.Millisecond * time.Duration(settings.DownloadDelay))
 }
