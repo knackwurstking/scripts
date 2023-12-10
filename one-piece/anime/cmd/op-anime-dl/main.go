1cb6c6f59f649ea32e0a6cc169a5d05dc36b1d0c
Udo Bauer <knackwurstking.tux@gmail.com>
2023-12-10 22:11:53 +0100

    remove config load from main and add flags parser

--- one-piece/anime/cmd/op-anime-dl/main.go
+++ one-piece/anime/cmd/op-anime-dl/main.go
@@ -2,21 +2,11 @@ package main
 
 import (
 	"flag"
-	"log/slog"
-	"path/filepath"
-)
-
-var (
-    configPath = filepath.Join("data", "config.json")
+	"time"
 )
 
 func main() {
-    // TODO: load config
     c := NewConfig()
-    if err := c.LoadFromFile(configPath); err != nil {
-        slog.Warn("Load config failed!", "err", err.Error())
-    }
-
     parseFlags(c)
 
     // TODO: fetch anime list, before entering the main loop
@@ -24,9 +14,56 @@ func main() {
 }
 
 func parseFlags(c *Config) {
-    // TODO: read flags: "debug", "config", ...
+    flag.BoolVar(&c.Debug, "debug", c.Debug, "Enable debugging")
+
+    flag.IntVar(
+        &c.Download.Delay.Hour,
+        "delay-hour",
+        c.Download.Delay.Hour,
+        "Set delay between downloads",
+    )
+
+    flag.IntVar(
+        &c.Download.Delay.Minute,
+        "delay-minute",
+        c.Download.Delay.Minute,
+        "Set delay between downloads",
+    )
+
+    flag.IntVar(
+        &c.Download.Delay.Second,
+        "delay-second",
+        c.Download.Delay.Second,
+        "Set delay between downloads",
+    )
+
+    flag.StringVar(
+        &c.Download.Dst,
+        "dst",
+        c.Download.Dst,
+        "Set destination path for downloads",
+    )
+
+    flag.IntVar(
+        &c.Download.LimitPerDay,
+        "limit",
+        c.Download.LimitPerDay,
+        "Download limit (per day)",
+    )
+
+    weekDay := flag.Int("update-on-day", int(c.Update.WeekDay),
+        "Weekday for update the anime list (0-6)")
+
+    hour := flag.Int("update-hour", c.Update.Hour,
+        "Hour (0-23) for anime list update")
 
     flag.Parse()
 
-    // TODO: merge flags to `*Config`
+    if *weekDay >= 0 && *weekDay <= 6 {
+        c.Update.WeekDay = time.Weekday(*weekDay)
+    }
+
+    if *hour >= 0 && *hour <= 23 {
+        c.Update.Hour = *hour
+    }
 }
