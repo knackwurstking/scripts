13ff65aef6b4cbde4a3169aa1a001c88a4ac01eb
Udo Bauer <knackwurstking.tux@gmail.com>
2023-12-10 10:41:57 +0100

    add todos

--- one-piece/anime/cmd/op-anime-dl/main.go
+++ one-piece/anime/cmd/op-anime-dl/main.go
@@ -1,5 +1,7 @@
 package main
 
 func main() {
-	// ...
+    // TODO: load config
+    // TODO: fetch anime list, before entering the main loop
+    // TODO: main loop, update every sunday @ 18:00
 }
