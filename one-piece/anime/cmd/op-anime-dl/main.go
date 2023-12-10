3df9d44d0006b9c877d276c1beb5b197f452ad2f
Udo Bauer <knackwurstking.tux@gmail.com>
2023-12-10 21:45:57 +0100

    add todos: parse flags

--- one-piece/anime/cmd/op-anime-dl/main.go
+++ one-piece/anime/cmd/op-anime-dl/main.go
@@ -1,13 +1,32 @@
 package main
 
-import "log/slog"
+import (
+	"flag"
+	"log/slog"
+	"path/filepath"
+)
+
+var (
+    configPath = filepath.Join("data", "config.json")
+)
 
 func main() {
     // TODO: load config
     c := NewConfig()
-    if err := c.LoadFromFile("config.json"); err != nil {
+    if err := c.LoadFromFile(configPath); err != nil {
         slog.Warn("Load config failed!", "err", err.Error())
     }
+
+    parseFlags(c)
+
     // TODO: fetch anime list, before entering the main loop
     // TODO: main loop, update every sunday @ 18:00
+}
+
+func parseFlags(c *Config) {
+    // TODO: read flags: "debug", "config", ...
+
+    flag.Parse()
+
+    // TODO: merge flags to `*Config`
 }
