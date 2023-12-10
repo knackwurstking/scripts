865d04c66b42146a5c172d50a821be2f6a1db5ff
Udo Bauer <knackwurstking.tux@gmail.com>
2023-12-10 21:39:16 +0100

    main: init and load config

--- one-piece/anime/cmd/op-anime-dl/main.go
+++ one-piece/anime/cmd/op-anime-dl/main.go
@@ -1,7 +1,13 @@
 package main
 
+import "log/slog"
+
 func main() {
     // TODO: load config
+    c := NewConfig()
+    if err := c.LoadFromFile("config.json"); err != nil {
+        slog.Warn("Load config failed!", "err", err.Error())
+    }
     // TODO: fetch anime list, before entering the main loop
     // TODO: main loop, update every sunday @ 18:00
 }
