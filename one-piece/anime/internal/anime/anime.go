d1a06ee6ca22a2d9e4f86793c177149cd84e6817
Udo Bauer <knackwurstking.tux@gmail.com>
2023-12-11 07:20:47 +0100

    add `anime.Anime`

---
+++ one-piece/anime/internal/anime/anime.go
@@ -0,0 +1,33 @@
+package anime
+
+import "fmt"
+
+type Chapter struct {
+}
+
+type Name string
+
+type Anime struct {
+	Origin string
+}
+
+func New(origin string) *Anime {
+	return &Anime{
+		Origin: origin,
+	}
+}
+
+func (anime *Anime) GetUrl(name Name) string {
+	switch name {
+    case "anime-list":
+        return fmt.Sprintf("%s/anime-list", anime.Origin)
+	default:
+		panic(fmt.Sprintf("Name \"%s\" not found!", name))
+	}
+}
+
+func (anime *Anime) GetAnimeList() ([]Chapter, error) {
+	// TODO: fetch anime list
+
+	return make([]Chapter, 0), nil
+}
